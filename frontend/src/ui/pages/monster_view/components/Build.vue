f7b3ae37ce4e12c95bfa8ecb2b9650664608576a
Peter Andersen <peter@porksmash.com>
2020-05-12 21:07:11 -0700

    Build component

---
+++ frontend/src/ui/pages/monster_view/components/Build.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <Runes :instance="instance" />
+    <Stats :instance="instance" />
+  </div>
+</template>
+
+<script>
+import Runes from "./Runes";
+import Stats from "./Stats";
+
+export default {
+  props: {
+    instance: { type: Object, required: true }
+  },
+  components: {
+    Runes,
+    Stats
+  }
+};
+</script>
+
+<style>
+</style>
